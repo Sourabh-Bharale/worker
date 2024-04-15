a758beef7446212697e81149323a57ef56cd34eb
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-04-15 14:12:39 +0530

    fix: set output vars for actions

--- src/app/api/actions/pr/route.ts
+++ src/app/api/actions/pr/route.ts
@@ -1,5 +1,9 @@
-export async function POST(req:any,res:any) {
-    const {message} = await req.body;
-    console.log(message)
-    return new Response(JSON.stringify({message}))
+export async function POST(req: any, res: any) {
+    const { comment_body,
+        comment_url,
+        pr_url } = await req.json();
+    console.log('Comment Body:', comment_body);
+    console.log('Comment URL:', comment_url);
+    console.log('PR URL:', pr_url);
+    return new Response(JSON.stringify({ message: 'success' }), { status: 200 })
 }
