35214a89efc6a5ec2bae091e45cdc2bb407c6998
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-04-16 12:45:58 +0530

    refactor

--- src/app/api/actions/pr/route.ts
+++ src/app/api/actions/pr/route.ts
@@ -1,5 +1,6 @@
 export async function POST(req: any, res: any) {
-    const { comment_body, comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents, branch, diff_hunk } = await req.json();
+    const { comment_body,
+         comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents, branch, diff_hunk } = await req.json();
     const decodedFileContents = file_contents.replace(/\\n/g, '\n')
         .replace(/\\r/g, '\r')
         .replace(/\\t/g, '\t')
