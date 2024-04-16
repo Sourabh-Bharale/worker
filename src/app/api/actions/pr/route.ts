b75cff1648b583da9f0128e2fc95a84752096625
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-04-16 12:52:56 +0530

    updated GHA

--- src/app/api/actions/pr/route.ts
+++ src/app/api/actions/pr/route.ts
@@ -1,13 +1,7 @@
 export async function POST(req: any, res: any) {
     const { comment_body,
          comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents, branch, diff_hunk } = await req.json();
-    const decodedFileContents = file_contents.replace(/\\n/g, '\n')
-        .replace(/\\r/g, '\r')
-        .replace(/\\t/g, '\t')
-        .replace(/\\b/g, '\b')
-        .replace(/\\f/g, '\f')
-        .replace(/\\\\/g, '\\')
-        .replace(/\\\"/g, '"');
+         const decodedFileContents = JSON.parse(`"${file_contents.replace(/\\"/g, '"')}"`);
 
     console.log({ comment_body, comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents: decodedFileContents, branch, diff_hunk });
 
