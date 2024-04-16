8cc07ed7ccdd9933876961bf7d53b3639e697203
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-04-16 12:53:53 +0530

    refactor route

--- src/app/api/actions/pr/route.ts
+++ src/app/api/actions/pr/route.ts
@@ -1,7 +1,7 @@
 export async function POST(req: any, res: any) {
     const { comment_body,
-         comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents, branch, diff_hunk } = await req.json();
-         const decodedFileContents = JSON.parse(`"${file_contents.replace(/\\"/g, '"')}"`);
+        comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents, branch, diff_hunk } = await req.json();
+    const decodedFileContents = JSON.parse(`"${file_contents.replace(/\\"/g, '"')}"`);
 
     console.log({ comment_body, comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, file_contents: decodedFileContents, branch, diff_hunk });
 
