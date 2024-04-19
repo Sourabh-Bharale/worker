95996d4c17364a736dc0585f1d4d0f5be3551ef5
Sourabh-Bharale <sourabhbharale80@gmail.com>
2024-04-19 14:05:24 +0530

    refactor: to fetch file content on server

--- src/app/api/actions/pr/route.ts
+++ src/app/api/actions/pr/route.ts
@@ -1,9 +1,16 @@
-import { GithubRepoLoader } from "langchain/document_loaders/web/github";
-
 export async function POST(req: any, res: any) {
     const { comment_body,
-        comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, branch, diff_hunk , file_content } = await req.json();
-    console.log({ comment_body, comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, branch, file_content, diff_hunk });
+        comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, file_line, branch, diff_hunk } = await req.json();
+
+    // get file content
+    const fileRes = await fetch(`https://raw.githubusercontent.com/${repo}/${branch}/${file_path}`,{
+        headers:{
+            'Authorization': `bearer ${process.env.GITHUB_API_TOKEN}`,
+        }
+    })
+    const fileContent = await fileRes.text()
+
+    console.log({ comment_body, comment_url, pr_url, repo, pr_number, pr_title, pr_body, pr_author, file_path, file_url, fileContent, file_line, branch, diff_hunk });
 
     return new Response(JSON.stringify({ message: 'success' }), { status: 200 });
 }
